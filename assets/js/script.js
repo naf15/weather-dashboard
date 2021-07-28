3f12fe607d12e8570336fed7e3a019089679bc7d
Nafis Rahman <nmr278@nyu.edu>
2021-07-27 22:32:46 -0400

    Updated cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,42 @@ var todaysDate = moment().format('MMM Do, YYYY');
 dateContainer.text(todaysDate);
 
 console.log(todaysDate)
+
+
+
+
+/*=====================
+WIDGETS
+=====================*/
+
+// Autocomplete
+
+$( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#city-input" ).autocomplete({
+      source: availableTags
+    });
+  } );
