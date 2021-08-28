129cd18e9101293af0a0c874575d21850698c020
Nafis Rahman <nmr278@nyu.edu>
2021-08-28 13:05:14 -0400

    Added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,181 @@
-var dateContainer = $('#date');
+/*=====================
+DEPENDENCIES
+=====================*/
 
-var todaysDate = moment().format('MMM Do, YYYY');
-
-dateContainer.text(todaysDate);
-
-console.log(todaysDate)
-
+var dateContainer = $('#city-date');
+var historyContainerEl = $('#search-history');
+var searchFormEl = $('#form');
+var searchBarEl = $('#search-city');
+var todaysTempEl = $('#temp');
+var todaysWindEl = $('#wind');
+var todaysHumidityEl = $('#humidity')
+var todaysUvEl = $('#uv');
 
 
 
 /*=====================
-WIDGETS
+DATA
+=====================*/
+var cityName = '';
+var searchHistory = [];
+var todaysDate = moment().format('MMM Do, YYYY');
+var defaultCity = 'New York City';
+var lat = 0;
+var lon = 0;
+
+var cityName = '';
+var apiKey = '76272cbf5adfd2b8a447adeb2a6997c1';
+
+
+var temperature = '';
+var windSpeeds = '';
+var humidity = '';
+var uvIndex = ''; 
+var fiveDayStartDate = moment();
+var fiveDatStartDay = moment().day();
+var daysOfTheWeek = {
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+  7: 'Sunday'
+}
+
+
+/*=====================
+FUNCTIONS
 =====================*/
 
-// Autocomplete
+function renderHistory() {
+  historyContainerEl.text('')
+  for (city of searchHistory) {
+    var historyEl = $('<button>')
+    historyEl.attr('class',"list-group-item list-group-item-action list-group-item-dark text-center");
+    historyEl.attr('value', city).text(city);
 
-$( function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#city-input" ).autocomplete({
-      source: availableTags
-    });
-  } );
+    historyContainerEl.append(historyEl);
+  };
+};
+
+async function getGeoLocation(cityName) {
+
+  var url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&units=imperial&appid=${apiKey}`;
+  
+  await fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      lon = data[0].lon;
+      lat = data[0].lat;
+      dateContainer.text(`${defaultCity} - ${todaysDate}`);
+      save(defaultCity)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+};
+
+async function getTodaysWeather(cityName) {
+  var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+  await fetch(url)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      console.log(data)
+      temperature = data.current.temp;
+      windSpeeds = data.current.wind_speed;
+      humidity = data.current.humidity;
+      uvIndex = data.current.uvi; 
+
+      todaysTempEl.text(temperature);
+      todaysWindEl.text(windSpeeds);
+      todaysHumidityEl.text(humidity);
+      todaysUvEl.text(uvIndex);
+    })
+};
+
+async function getFiveDayWeather(cityName) {
+  var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+  await fetch(url)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      var hourStepSize = 24/3;
+      for (var i=0; i<data.list.length; i++) {
+        var dayStats = data.list[i*hourStepSize];
+
+        if (i > 0){
+          fiveDayStartDate = fiveDayStartDate.add(1,'d') 
+        }
+        
+
+        $(`#date-${i+1}`).text(fiveDayStartDate.format('MMM Do, YYYY'));
+        $(`#day-${i+1}`).text(daysOfTheWeek[(fiveDatStartDay + i - 1) % 7 + 1]);
+
+        $(`#day-${i+1}-stat-1-value`).text(dayStats.main.temp.toString().slice(0,4));
+        $(`#day-${i+1}-stat-2-value`).text(dayStats.wind.speed.toString().slice(0,4));
+        $(`#day-${i+1}-stat-3-value`).text(dayStats.main.humidity.toString().slice(0,4));
+        
+        // `https://openweathermap.org/img/wn/${dayStats.weather.icon}@2x.png`
+      }
+    })
+}
+
+function save (city) {
+  // get from local if exits
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+  }
+  localStorage.setItem('Search History',JSON.stringify(searchHistory));
+};
+
+function getCityInput () {
+  searchFormEl.on('submit', (event) => {
+    event.preventDefault(); 
+    var cityInput = searchBarEl.val();
+    searchBarEl.val('');
+    defaultCity = cityInput;
+    getGeoLocation(defaultCity);
+    getSearchHistory();
+    renderHistory();
+  });
+};
+
+function getSearchHistory() {
+  searchHistory = JSON.parse(localStorage.getItem("Search History"));
+  if (!searchHistory) {
+    searchHistory = [];
+  };
+};
+
+/*=====================
+INITIALIZATION
+=====================*/
+
+function init () {
+  getCityInput();
+  getGeoLocation(defaultCity);
+  getTodaysWeather(defaultCity);
+  getSearchHistory();
+  renderHistory();
+  getFiveDayWeather(defaultCity);
+
+  
+  // renderTodaysWeather(defaultCity);
+  // renderWeeklyWeather(defaultCity);
+};
+
+init();
+
+historyContainerEl.on('click', (event) => {
+  event.preventDefault();
+  defaultCity = event.target.value
+  init();
+});
